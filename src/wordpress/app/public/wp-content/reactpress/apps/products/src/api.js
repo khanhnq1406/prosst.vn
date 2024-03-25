8bbea6cd48f71589a5f03bbcc2ee60d3531d8a92
Quốc Khánh Nguyễn <khanhnq1406@gmail.com>
2024-03-25 19:59:25 +0700

    PROSST-27: Fetch product with categories
    - Get categories name via ID
    - Get product list with categories
    
    Signed-off-by: Quốc Khánh Nguyễn <khanhnq1406@gmail.com>

--- src/wordpress/app/public/wp-content/reactpress/apps/products/src/api.js
+++ src/wordpress/app/public/wp-content/reactpress/apps/products/src/api.js
@@ -1,14 +1,38 @@
 import { useEffect, useState } from "react";
-const WPURL = "http://localhost:10004/wp-json/wp/v2/posts";
+const WPURL = "http://localhost:10004/wp-json/wp/v2";
 export async function postsApi() {
+  const url = `${WPURL}/posts`;
   try {
-    const response = await fetch(WPURL);
+    const response = await fetch(url);
     if (!response.ok) {
       // oups! something went wrong
       return;
     }
     const posts = await response.json();
     return posts;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getCategoryId(slug) {
+  const url = `${WPURL}/categories`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      // oups! something went wrong
+      return;
+    }
+    const responseJson = await response.json();
+    let id = "";
+    for (let category of responseJson) {
+      if (slug === category.slug) {
+        id = category.id;
+        break;
+      }
+    }
+    return id;
   } catch (error) {
     console.error(error);
     return [];
--- src/wordpress/app/public/wp-content/reactpress/apps/products/src/routes/root.jsx
+++ src/wordpress/app/public/wp-content/reactpress/apps/products/src/routes/root.jsx
@@ -1,33 +1,65 @@
-import { postsApi } from "../api";
+import { getCategoryId, postsApi } from "../api";
 import { useEffect, useState } from "react";
 
 export default function Root() {
   const [postsInfo, setPostsInfo] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const posts = await postsApi();
-        posts.forEach(async (post) => {
-          const imgUrl = post._links["wp:attachment"][0].href;
-          const imgResponse = await fetch(imgUrl);
-          let imgPath = await imgResponse.json();
-          imgPath = imgPath[0].source_url;
-          setPostsInfo((prevPost) => {
-            const found = prevPost.find((element) => element.id === post.id);
-            if (found !== undefined) return [...prevPost];
-            console.log(imgPath);
-            return [
-              ...prevPost,
-              { id: post.id, title: post.title.rendered, path: imgPath },
-            ];
-          });
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const posts = await postsApi();
+  //       const categorieId = await getCategoryId("banh-rang");
+  //       for (let post of posts) {
+  //         if (post.categories[0] !== categorieId) continue;
+  //         const imgUrl = post._links["wp:attachment"][0].href;
+  //         const imgResponse = await fetch(imgUrl);
+  //         let imgPath = await imgResponse.json();
+  //         imgPath = imgPath[0].source_url;
+  //         console.log(post.categories[0] !== categorieId);
+  //         setPostsInfo((prevPost) => {
+  //           const found = prevPost.find((element) => element.id === post.id);
+  //           if (found !== undefined) return [...prevPost];
+  //           console.log(post);
+  //           return [
+  //             ...prevPost,
+  //             { id: post.id, title: post.title.rendered, path: imgPath },
+  //           ];
+  //         });
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //       alert("Error loading posts");
+  //     }
+  //   })();
+  // }, []);
+
+  const getPosts = async (slug) => {
+    try {
+      setPostsInfo([]);
+      const posts = await postsApi();
+      const categorieId = await getCategoryId(slug);
+      for (let post of posts) {
+        if (post.categories[0] !== categorieId) continue;
+        const imgUrl = post._links["wp:attachment"][0].href;
+        const imgResponse = await fetch(imgUrl);
+        let imgPath = await imgResponse.json();
+        imgPath = imgPath[0].source_url;
+        console.log(post.categories[0] !== categorieId);
+        setPostsInfo((prevPost) => {
+          const found = prevPost.find((element) => element.id === post.id);
+          if (found !== undefined) return [...prevPost];
+          console.log(post);
+          return [
+            ...prevPost,
+            { id: post.id, title: post.title.rendered, path: imgPath },
+          ];
         });
-      } catch (err) {
-        alert("Error loading posts");
       }
-    })();
-  }, []);
+    } catch (err) {
+      console.log(err);
+      alert("Error loading posts");
+    }
+  };
   const postItems = postsInfo.map((post) => (
     <li key={post.id}>
       <p>{post.title}</p>
@@ -38,6 +70,8 @@ export default function Root() {
     <>
       <h1>Root</h1>
       <ul>{postItems}</ul>
+      <button onClick={(e) => getPosts("banh-rang")}>Banh rang</button>
+      <button onClick={(e) => getPosts("xich-tai")}>Xich tai</button>
     </>
   );
 }
